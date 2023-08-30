d8255d60b003a8cd463164044f676f4c54f0b176
harishdharmavaram <44495956+harishdharmavaram@users.noreply.github.com>
2023-08-30 17:49:14 +0530

    Update test_snowflake_connector.py

--- test_snowflake_connector.py
+++ test_snowflake_connector.py
@@ -8,16 +8,19 @@ from snowflake_connector import SnowflakeConnector
 def snowflake_connection():
     # Set up the Snowflake connection here, return the connection object
 
-    with open("secret_dev.json") as secrets_file:
-        secrets = json.load(secrets_file)
+    # with open("secret_dev.json") as secrets_file:
+     #   secrets = json.load(secrets_file)
 
-    user = secrets["snowflake"]["snowflake_user"]
-    password = secrets["snowflake"]["snowflake_password"]
-    warehouse = secrets["snowflake"]["snowflake_warehouse"]
-    database = secrets["snowflake"]["snowflake_database"]
-    schema = secrets["snowflake"]["snowflake_schema"]
-    account = secrets["snowflake"]["snowflake_account"]
-
+   connection_parameters ={
+    "user" : "Aklesh",
+    "password" : "Ran@_123",
+    "warehouse" : "COMPUTE_WH",
+    "database" : "DEMO",
+    "schema" : "PUBLIC",
+    "account" : "yudedse-av63812",
+    "role" : "ACCOUNTADMIN",
+    "stage" : "STG"
+}
     connection = SnowflakeConnector(user, password, account, warehouse, database, schema)
     connection.connect()
 
