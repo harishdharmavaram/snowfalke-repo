2eaa8b4e20c38c0aad32d42df2b4a5afe92cc6a8
harishdharmavaram <44495956+harishdharmavaram@users.noreply.github.com>
2023-08-30 22:42:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,3 @@
-from snowflake.snowpark.functions import udf
-from snowflake.snowpark.types import IntegerType
-import json
-from snowflake.snowpark import Session
-
-
-connection_parameters = {
-  "account": "yudedse-av63812",
-  "user": "Aklesh",
-  "password": "Ran@_123",
-  "role": "ACCOUNTADMIN",
-  "warehouse": "COMPUTE_WH",
-  "database": "DEMO",
-  "schema": "PUBLIC",
-  "stage": "STG"}
-
-session = Session.builder.configs(connection_parameters).create()
-
-
-@udf(name='xyz', input_types=[IntegerType(), IntegerType()], return_type=IntegerType(), stage_location='@STG', is_permanent=True, replace=True)
-def xyz(a: int, b: int) -> int:
-    return a-b
+def add():
+    result = 5 + 5
+    return result
